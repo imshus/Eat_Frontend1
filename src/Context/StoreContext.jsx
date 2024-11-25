eb606e068e188c37e81f67a6a4b3b76f4e66bf04
imshus <67273421+imshus@users.noreply.github.com>
2024-11-26 00:01:30 +0530

    Update StoreContext.jsx

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 
 const StoreContextProvider = (props) => {
-    const url="http://localhost:4000"
+    const url="https://eat-backend-oa1ci4w3o-imshus-projects.vercel.app"
 
     
     const [food_list, setFoodList] = useState([]);
